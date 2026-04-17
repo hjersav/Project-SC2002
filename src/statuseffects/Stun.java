a87edc509a1e4db205a2f8407ce04ebd58c82515
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 03:13:49 +0800

    Update Stun.java

--- src/statuseffects/Stun.java
+++ src/statuseffects/Stun.java
@@ -1,1 +1,17 @@
+package statuseffects;
+public class Stun extends StatusEffect{
+  public Stun(int duration){
+    this.name ="Stun";
+    this.duration=duration;
+  }
 
+  @Override
+  public void apply(Object target){
+    System.out.println(target + " is stunned and canot act!");
+  }
+
+  @Override
+  public String getDescription(){
+    return "Stunned: cannot take actions for " + duration + " turn(s).";
+  }
+}
