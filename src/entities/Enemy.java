1e56df74fe9aa446f140465b73447432a85be20e
anjali kethireddy <anjalikethireddy777@gmail.com>
2026-04-18 01:39:05 +0800

    Update Enemy.java

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -1,29 +1,3 @@
-// Combatant Class - Base class for all characters (players & enemies)
-public abstract class Combatant {
-    private String name;
-    private int health;
-    private int attack;
-    private int defense;
-    private int speed;
-
-    // Constructor
-    public Combatant(String name, int health, int attack, int defense, int speed) {
-        this.name = name;
-        this.health = health;
-        this.attack = attack;
-        this.defense = defense;
-        this.speed = speed;
-    }
-
-    // Common combat methods
-    public abstract void attack(Combatant target); // Abstract method for attack
-    public void takeDamage(int damage) { this.health -= damage; }
-    public boolean isAlive() { return this.health > 0; }
-
-    // Getters and setters (omitted for brevity)
-}
-
-// Enemy Class - A specialized subclass for enemies
 public class Enemy extends Combatant {
 
     public Enemy(String name, int health, int attack, int defense, int speed) {
