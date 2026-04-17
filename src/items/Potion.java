c24a2b324448ab6de206ac85c24b0ac0cdf7d024
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 01:39:10 +0800

    Update Potion.java

--- src/items/Potion.java
+++ src/items/Potion.java
@@ -1,1 +1,17 @@
-
+package items;
+import entities.Player;
+public class Potion extends Item {
+  public Potion() {
+    this.name = "Potion";
+  }
+  @Override
+  public void use(Object user){
+    if (user instanceof Player){
+      Player p=(Player) user;
+      int newHp=Math.min(p.getHP() + 100, p.getMaxHP());
+      p.setHP(newHp);
+      used=true;
+      System.out.print(p.getName() + " used Potion! HP restored to " + newHp);
+    }
+  }
+}
