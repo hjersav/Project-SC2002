5633bcb9f2fcdcc51ade5643d0c9d045e30fe086
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 03:09:57 +0800

    Update StatusEffect.java

--- src/statuseffects/StatusEffect.java
+++ src/statuseffects/StatusEffect.java
@@ -1,1 +1,18 @@
+package statuseffects;
+public abstract class StatusEffect{
+  protected String name;
+  protected int duration;
 
+  public String getName() {return name;}
+  public int getDuration() {return duration;}
+
+  public void tick(){
+    if (duration>0) duration--;
+  }
+  public boolean isExpired(){
+    return duration <=0;
+  }
+
+  public abstract void apply(Object target);
+  public abstract String getDescription();
+}
