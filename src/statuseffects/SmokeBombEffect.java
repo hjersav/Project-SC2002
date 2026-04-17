db5a9ab1a74970481fd3f81b289db411c25282de
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 03:18:18 +0800

    Update SmokeBombEffect.java

--- src/statuseffects/SmokeBombEffect.java
+++ src/statuseffects/SmokeBombEffect.java
@@ -1,1 +1,19 @@
+package statuseffects;
+import entities.Player;
+public class SmokeBombEffect extends StatusEffect{
+  public SmokeBombEffect(int duration){
+    this.name="Smoke Bomb";
+    this.duration=duration;
+  }
 
+  @Override
+  public void apply(Object target){
+    System.out.println("Smoke Bomb active! Enemy attacks deal 0 damage.");
+  }
+
+  @Override
+  public String getDescriptioin(){
+    return "Smoke Bomb: enemy attacks deal 0 damage for " + duration + " turn(s).";
+  }
+}
+    
