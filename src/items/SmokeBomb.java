17ed37aa3769abc059d11cde32192919c3946018
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 01:46:36 +0800

    Update SmokeBomb.java

--- src/items/SmokeBomb.java
+++ src/items/SmokeBomb.java
@@ -1,1 +1,15 @@
+package items;
+import entities.Player;
+public class SmokeBomb extends Item{
+  public SmokeBomb(){
+    this.name = "SmokeBomb";
+  }
 
+  @Override
+  public void use(Object user){
+    if (user instanceof Player){
+      Player p = (Player) user;
+      p.applyStatusEffect(new statuseffects.SmokeBombEffect(2));
+    }
+  }
+}
