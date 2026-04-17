8dd746dd46f35bfdf200ee40b5e21b40905ee859
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 01:43:50 +0800

    Update PowerStone.java

--- src/items/PowerStone.java
+++ src/items/PowerStone.java
@@ -1,1 +1,15 @@
+package items;
+import entities.Player;
+public class PowerStone extends Item{
+  public PowerStone(){
+    this.name = "PowerStone";
+  }
 
+  @Override
+  public void use(Object user) {
+    if (user instanceof Player) {
+      Player p =(Player) user;
+      p.triggerSpecialSkillOnce();
+    }
+  }
+}
