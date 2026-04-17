f889c99943d839fbec3e4612424ee06bba7aa535
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 01:48:55 +0800

    Update SmokeBomb.java

--- src/items/SmokeBomb.java
+++ src/items/SmokeBomb.java
@@ -10,6 +10,8 @@ public class SmokeBomb extends Item{
     if (user instanceof Player){
       Player p = (Player) user;
       p.applyStatusEffect(new statuseffects.SmokeBombEffect(2));
+      used = true;
+      System.out.println(p.getName() + " used Smoke Bomb! Enemy attacks deal 0 damage for 2 turns.");
     }
   }
 }
