dbed31ab4c28f287709f41f2e876463943b4541d
vibushaharibaskar-arch <vibushaharibaskar@gmail.com>
2026-04-18 01:34:57 +0800

    Update Item.java

--- src/items/Item.java
+++ src/items/Item.java
@@ -1,1 +1,8 @@
-
+package items;
+public abstract class Item{
+  protected String name;
+  protected boolean used = false;
+  public String getName() {return name;}
+  public boolean isUsed() {return used;}
+  public abstract void use(Object user);
+}
